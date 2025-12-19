6050e34e86de73b048672e84162d779140405463
Kornel Pustelak <92749743+FilozoFja@users.noreply.github.com>
2025-12-19 15:42:08 +0000

    post-publishing-validation-hotfix

--- MVCBlogApp/Models/ViewModel/Post/EditPostView.cs
+++ MVCBlogApp/Models/ViewModel/Post/EditPostView.cs
@@ -6,10 +6,7 @@ namespace MVCBlogApp.Models.ViewModel.Post;
 
 public class EditPostView
 {
-    [Required]
     public List<PostCategory> PostCategories { get; set; } = new List<PostCategory>();
-    [Required]
     public AddOrEditPostDto? PostDto { get; set; } = new AddOrEditPostDto();
-    [Required]
     public int? EditedPostId { get; set; }
 }
